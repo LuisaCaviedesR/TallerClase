c83a9bd9675b3e036548d7bf3b0df8e187ff9e8f
Estudiante <estuam@UAM.LOCAL>
2017-02-25 16:06:20 -0500

    Proyecto con formularios Luisa y Estefania

--- resources/views/Form/FormLuisa.blade.php
+++ resources/views/Form/FormLuisa.blade.php
@@ -1,9 +1,4 @@
 {!! Form::open(['url' => 'foo/bar']) !!}
-<<<<<<< HEAD
-{!! Form::label('email' , 'E-mail Address') !!}
-{!! Form::text('email' , 'Luisacaviedes@gmail.com'); !!}
-{!! Form::checkbox('name' , 'value'); !!}
-=======
 {!! Form::label('pregunta1' , '¿Qué le gusta más en nuestro nuevo servicio?') !!}<br>
 {!! Form::textarea('pregunta1' , '') !!}
 {!! Form::checkbox('name' , 'value') !!}
