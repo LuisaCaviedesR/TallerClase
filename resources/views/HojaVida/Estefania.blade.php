620bbb6d0f04f3329e893008bad8937cae8053d6
Estudiante <estuam@UAM.LOCAL>
2017-02-25 15:01:33 -0500

    Hoja vida Estefania

---
+++ resources/views/HojaVida/Estefania.blade.php
@@ -0,0 +1,28 @@
+@extends('Layout.bio')
+
+@section('Foto')
+
+@stop
+
+@section('InformacionPersonal')
+
+    Nombre: Estefanía
+    Apellidos: Marín Puerta
+    Fecha Nacimiento: 11 Septiembre 1990
+    Celular: 3104582820
+    Lugar de Residencia: Manizales - Caldas
+
+@stop
+
+@section('InformacionAcademica')
+
+    Primaria: Escuela Antonia Santos
+    Secundaria: Instituto Universitario de Caldas
+    Pregrado: Ingeniería de sistemas y computación - Universidad de Caldas
+    Postgrado: Especialización en ingeniería de requerimientos - Universidad Autonoma de Manizales
+
+@stop
+
+@section('ExperienciaLaboral')
+    Webde
+@stop
