f0f93f1627fdfdb011cbdc35c514dce2926f10a1
unknown <estuam@SALAF402-23.UAM.LOCAL>
2017-02-25 14:52:33 -0500

    Enlaces

---
+++ resources/views/HojaVida/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+@section('contenido')
+
+A continuación Encontrara las hojas de vida de las integrantes del Grupo:
+
+    <a href="url" > Luisa</a>
+    <a href="url" > Erika</a>
+    <a href="url" > Estefania</a>
+
+@stop
