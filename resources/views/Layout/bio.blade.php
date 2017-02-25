943c1e66df1b959eff1535071ac21ed908951c4f
Estudiante <estuam@UAM.LOCAL>
2017-02-25 14:48:04 -0500

    layoutBio

---
+++ resources/views/Layout/bio.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <title>Taller Hojas de Vida</title>
+        <meta name="description" content="Desarrollo Web">
+        <meta name="author" content="Erika Estefania Luisa">
+    </head>
+    <body>
+        <div>@yield('Foto')</div>
+        <div>@yield('InformacionPersonal')</div>
+        <div>@yield('InformacionAcademica')</div>
+        <div>@yield('ExperienciaLaboral')</div>
+    </body>
+</html>
