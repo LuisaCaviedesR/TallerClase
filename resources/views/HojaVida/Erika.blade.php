79aa03d79c3098f4df31787383577d52b45d512f
unknown <estuam@SALAF402-23.UAM.LOCAL>
2017-02-25 15:29:28 -0500

    Erika ok

--- resources/views/HojaVida/Erika.blade.php
+++ resources/views/HojaVida/Erika.blade.php
@@ -1,24 +1,24 @@
-@extends('Layout.bio')
+@extends ('Layout.bio')
 
-@section('Foto')
+@section ('foto')
 @stop
 
-@section('InformacionPersonal')
-    Nombre: Erika Johana
+@section ('InformacionPersonal')
+Nombre: Erika Johana
     Apellidos: Caicedo Arias
     Fecha de Nacimiento: 28 de Julio de 1988
     Celular: 3147379679
 @stop
 
-@section('InformacionAcademica')
-    Primaria: La Santisima Trinidad
-    Bachiller: Colegio Maria Inmaculada
-    Pregrado: Ingeniero de Software
-    Postgrado: Maestria en Gerencia de Proyectos de Software
+@section ('InformacionAcademica')
+Primaria: La Santisima Trinidad
+Bachiller: Colegio Maria Inmaculada
+Pregrado: Ingeniero de Software
+Postgrado: Maestria en Gerencia de Proyectos de Software
 @stop
 
 
-@section('ExperienciaLaboral')
-    Docente de Pregrado
-    Directora de Programa
+@section ('Experiencia Laboral')
+Docente de Pregrado
+Directora de Programa
 @stop
