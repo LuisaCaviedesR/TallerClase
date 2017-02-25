b95b8565bafb5e4b2b1587a1f6f565a327fee65d
Estudiante <estuam@UAM.LOCAL>
2017-02-25 14:31:19 -0500

    add method

--- app/Http/Controllers/HojaVidaController.php
+++ app/Http/Controllers/HojaVidaController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class HojaVidaController extends Controller
 {
+
+    public function index(){
+       return view('HojaVida/index');
+    }
     //
+    public function hojaVida(Request $request,$nombre){
+        return view('HojaVida/');
+    }
 }
