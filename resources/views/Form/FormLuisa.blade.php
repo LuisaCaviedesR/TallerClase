e72cb9efae953d4045dacbe8d6f6a9445b424e10
Estudiante <estuam@UAM.LOCAL>
2017-02-25 15:55:12 -0500

    encuesta

--- resources/views/Form/FormLuisa.blade.php
+++ resources/views/Form/FormLuisa.blade.php
@@ -1,5 +1,5 @@
-{!! Form::open(['url']=> 'foo/bar') !!}
-{!! Form::label('email , 'E-mail Address'); !!}
-{!! Form::text('email , 'Luisacaviedes@gmail.com'); !!}
-{!! Form::checkbox('name , 'value'); !!}
-{!! Form::close(); !!}
+{!! Form::open(['url' => 'foo/bar']) !!}
+{!! Form::label('pregunta1' , '¿Qué le gusta más en nuestro nuevo servicio?') !!}<br>
+{!! Form::textarea('pregunta1' , '') !!}
+{!! Form::checkbox('name' , 'value') !!}
+{!! Form::close() !!}
