30b60b4bba442f3e94d4b6e04664a436910fa722
unknown <estuam@SALAF402-23.UAM.LOCAL>
2017-02-25 14:58:35 -0500

    Hoja vida erika

---
+++ resources/views/HojaVida/Erika.blade.php
@@ -0,0 +1,17 @@
+@extendes ('layout.bio')
+@section ('foto')
+@stop
+
+@section (InformacionPersonal')
+    Nombre: Erika Johana
+    Apellidos: Caicedo Arias
+    Fecha de Nacimiento: 28 de Julio de 1988
+    Celular: 3147379679
+@stop
+
+@section ('InformacionAcademica')
+@stop
+
+
+@section ('Experiencia Labiral)
+@stop
